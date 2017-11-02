e7c1d00fafa49b4e6ab7ddae587d055acbe94d78
SHI PENG <601011162@qq.com>
2017-11-02 12:35:24 +0800

    ftl

--- src/main/java/com/xq/controller/OrderController.java
+++ src/main/java/com/xq/controller/OrderController.java
@@ -1,19 +1,26 @@
 package com.xq.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 我的订单
  * Created by netlab606 on 2017/11/2.
  */
 @Controller
-@RequestMapping("/wx/order")
+@RequestMapping("")
 public class OrderController {
 
-    @RequestMapping(value = "",method = RequestMethod.GET)
-    public String order(){
-        return "index";
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public ModelAndView order(){
+        ModelAndView mv=new ModelAndView("order/index");
+        mv.addObject("username","你好,freemarker!");
+        return mv;
+//        model.addAttribute("username","aaa");
+//        return "index.ftl";
+
     }
 }
